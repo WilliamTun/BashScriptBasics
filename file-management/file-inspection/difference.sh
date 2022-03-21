86f9eb967cbf2c93ec45bc190031bef80cfe73bb
William Tun <williamtun@Williams-MacBook-Pro.local>
2022-03-21 17:55:53 +0000

    diff

--- file-management/file-inspection/difference.sh
+++ file-management/file-inspection/difference.sh
@@ -1,4 +1,9 @@
+
+# compare difference between two FILES
 head -n 4 states.txt > four.txt
 head -n 6 states.txt > six.txt
 
 sdiff four.txt six.txt
+
+# compare difference between two DIRECTORIES
+diff </path/directory1> </path/directory2>
